793a4a8d4fa87018e9d3400315d499ef0f72a50e
Leonardo Thieme <leonardothieme95@gmail.com>
2018-01-19 20:38:19 -0200

    Initial commit

---
+++ app/src/main/java/idadedecachorro/cursoandroid/com/idadedecachorro/MainActivity.java
@@ -0,0 +1,48 @@
+package idadedecachorro.cursoandroid.com.idadedecachorro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText caixaTexto;
+    private Button botaoIdade;
+    private TextView resultadoIdade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        caixaTexto = (EditText) findViewById(R.id.caixaTextoId);
+        botaoIdade = (Button) findViewById(R.id.botaoidadeId);
+        resultadoIdade = (TextView) findViewById(R.id.resultadoIdadeId);
+
+        botaoIdade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //recuperar o que foi digitado
+                String textoDigitado = caixaTexto.getText().toString();
+
+                if(textoDigitado.isEmpty()){
+                    resultadoIdade.setText("Nenhum número digitado");
+                }else {
+                    int valorDigitado = Integer.parseInt(textoDigitado);
+                    int valorResultadoFinal = valorDigitado * 7;
+
+                    resultadoIdade.setText("A idade do cachorro  em anos humanos é " + valorResultadoFinal + " anos");
+
+                }
+
+
+            }
+        });
+
+    }
+}
